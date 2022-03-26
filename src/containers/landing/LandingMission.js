3829b49a12a6ece8e57d5426204bf40dcd77861a
Sil Kreulen <silkreulen@gmail.com>
2022-03-26 17:41:41 +0100

    update text

--- src/containers/landing/LandingMission.js
+++ src/containers/landing/LandingMission.js
@@ -4,7 +4,7 @@ import { varFadeInUp, MotionInView } from "../../components/animate";
 
 const RootStyle = styled("div")(({ theme }) => ({
   position: "relative",
-  marginTop: theme.spacing(-12),
+  marginTop: theme.spacing(-10),
 
   [theme.breakpoints.up("sm")]: {
     marginTop: theme.spacing(-5)
@@ -25,13 +25,12 @@ const SectionTitle = styled((props) => <Typography variant="body2" {...props} />
 const SectionText = styled((props) => <Typography variant="body2" {...props} />)(({ theme }) => ({}));
 
 const SectionContainer = styled((props) => <Box {...props} />)(({ theme }) => ({
-  minHeight: "200px",
   background: alpha(theme.palette.primary.main, 0.1),
   borderRadius: theme.shape.borderRadiusMd,
   padding: theme.spacing(1, 2),
 
   [theme.breakpoints.up("md")]: {
-    minHeight: "265px",
+    minHeight: "310px",
     padding: theme.spacing(3, 2)
   }
 }));
@@ -61,11 +60,11 @@ export function LandingMission() {
             <SectionContainer>
               <SectionTitle>How does it work?</SectionTitle>
               <SectionText>
-                Whereas the collection has a funny tone, the cause is not. The intention of this initiative is not to downplay the seriousness of the war in Ukraine. Instead, we want to offer an alternative way to help Ukraine. All income will be distributed through{" "}
+                Though the collection has a funny tone, the cause is not. The intention of this initiative is not to downplay the seriousness of the war in Ukraine. Instead, we want to offer an alternative way to help the people of the country. All income will be distributed through{" "}
                 <a href="https://www.ukrainedao.love/" target="_blank" rel="noreferrer">
                   UkraineDao
                 </a>{" "}
-                to the people in Ukraine. UkraineDao is a blockchain initiative aimed at distributing donations to Ukraine.
+                to the people in Ukraine. UkraineDao is a blockchain initiative aimed at raising funds for Ukrainian organisations that are helping those suffering from the war.
               </SectionText>
             </SectionContainer>
           </GridItem>
@@ -73,7 +72,7 @@ export function LandingMission() {
             <SectionContainer>
               <SectionTitle>Is this not a scam?</SectionTitle>
               <SectionText>
-                Given the many scammers and false promises in the NFT space, we want to assure you that our intentions are genuine by having hardcoded that money withdrawn directly goes to the wallet of UkraineDao. This is immutable: see{" "}
+                Given the many scammers and false promises in the NFT space, we want to assure you that our intentions are genuine. Therefore, we have hardcoded that ether in the contract cannot be withdrawn by the owners, but only be send directly to the wallet of UkraineDao. This is immutable: see{" "}
                 <a href={`${process.env.NEXT_PUBLIC_EITHERSCAN_PREFIX}/address/${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}#code`} target="_blank" rel="noreferrer">
                   line 129 of the smart contract
                 </a>
@@ -84,7 +83,7 @@ export function LandingMission() {
           <GridItem>
             <SectionContainer>
               <SectionTitle>Why not just donate?</SectionTitle>
-              <SectionText>We aim to provide an alternative for helping the people of Ukraine and want to demonstrate that NFTs are a way to accomplish meaningful effects in the real world. If just one person would buy an NFT, that would otherwise have not donated, this project has fulfilled its purpose.</SectionText>
+              <SectionText>We aim to provide an alternative for helping the people of Ukraine and want to demonstrate that NFTs are a way to accomplish meaningful effects in the real world. If just one person would buy an NFT, who would otherwise not have donated, this project has fulfilled its purpose.</SectionText>
             </SectionContainer>
           </GridItem>
         </Grid>
